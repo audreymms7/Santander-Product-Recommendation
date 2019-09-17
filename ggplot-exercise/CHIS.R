ec041021bdd74db4bad35f9d052bc2f930c586e2
Audrey Meng <audrey.meng@cgic.cooperators.ca>
2019-09-17 16:16:13 -0400

    up

--- ggplot-exercise/CHIS.R
+++ ggplot-exercise/CHIS.R
@@ -14,3 +14,70 @@ ggplot(adult, aes(x=BMI_P))+
 # Age colored by BMI, binwidth = 1
 ggplot(adult, aes(x=SRAGE_P,fill=factor(RBMI)))+
   geom_histogram(binwidth=1)
+
+# Keep adults younger than or equal to 84
+adult <- adult[adult$SRAGE_P <= 84, ] 
+
+# Keep adults with BMI at least 16 and less than 52
+adult <- adult[adult$BMI_P >= 16 & adult$BMI_P< 52, ]
+
+# Relabel the race variable
+adult$RACEHPR2 <- factor(adult$RACEHPR2, labels = c("Latino", "Asian", "African American", "White"))
+
+# Relabel the BMI categories variable
+adult$RBMI <- factor(adult$RBMI, labels = c("Under-weight", "Normal-weight", "Over-weight", "Obese"))
+
+# The color scale used in the plot
+BMI_fill <- scale_fill_brewer("BMI Category", palette = "Reds")
+
+# Theme to fix category display in faceted plot
+fix_strips <- theme(strip.text.y = element_text(angle = 0, hjust = 0, vjust = 0.1, size = 14),
+                    strip.background = element_blank(),
+                    legend.position = "none")
+
+# Histogram, add BMI_fill and customizations
+ggplot(adult, aes (x = SRAGE_P, fill= RBMI)) + 
+  geom_histogram(binwidth = 1) +
+  fix_strips+
+  BMI_fill+
+  facet_grid(RBMI~.)+
+  theme_classic()
+
+# Plot 1 - Count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  BMI_fill
+
+# Plot 2 - Density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill
+
+# Plot 3 - Faceted count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
+  geom_histogram(binwidth = 1) +
+  BMI_fill+
+  facet_grid(RBMI~.)
+
+
+
+# Plot 4 - Faceted density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1) +
+  BMI_fill+
+  facet_grid(RBMI~.)
+
+
+
+
+# Plot 5 - Density histogram with position = "fill"
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..density..), binwidth = 1,position= "fill") +
+  BMI_fill
+
+
+
+# Plot 6 - The accurate histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..count../sum(..count..)), binwidth = 1,position= "fill") +
+  BMI_fill
